cb7a552d7a22e0243400e7e9acc1f5f7bc194bc7
Adilet312 <adiletm@hotmail.com>
2020-04-19 23:41:23 -0700

    Final version of ToDoList with functionalities delet single item, add single item, delete all items, and select item with mark.

--- to-do-list-with-react/src/components/Task.js
+++ to-do-list-with-react/src/components/Task.js
@@ -7,7 +7,7 @@ class Task extends React.Component{
     super();
     this.state = {
       newtask:'',
-      list:[{nameTask:'',done: false}],
+      list:[],
 
     }
   }
@@ -35,16 +35,16 @@ class Task extends React.Component{
   selectTask = (index) => {
     let tempList = [];
     tempList = this.state.list;
-    tempList[index].done = true;
+    tempList[index].done = !tempList[index].done ;
     console.log(tempList[index].done)
     this.setState({list:tempList});
   }
 
   render(){
-    const wholeList = this.state.list.map((e,idx) => <li key={idx} className={false}>
-                                                        <input onClick = {() => this.selectTask(idx)} type='checkBox'/>{e.nameTask}<span
-                                                        onClick = {() => this.deleteTask(idx)}  className='delete'>X</span>
-                                                      </li>);
+    // const wholeList = this.state.list.map((e,idx) => <li key={idx} className={false}>
+    //                                                     <input onClick = {() => this.selectTask(idx)} type='checkBox'/>{e.nameTask}<span
+    //                                                     onClick = {() => this.deleteTask(idx)}  className='delete'>X</span>
+    //                                                   </li>);
     return (
       <div className='container'>
         <h1>To Do List</h1>
@@ -54,7 +54,15 @@ class Task extends React.Component{
         </div>
         <div className='content'>
           <ul>
-            {wholeList}
+            {
+
+              this.state.list.map((e,idx) =>   {
+                let isDone = e.done ? "crossed" : '';
+                return (<li key={idx} className={isDone}><input onClick = {() => this.selectTask(idx)} type='checkBox'/>{e.nameTask}
+                            <span onClick = {() => this.deleteTask(idx)}  className='delete'>X</span>
+                        </li>)
+                      })
+            }
           </ul>
         </div>
         <button className='clear' onClick={this.clearList}>Clear</button>
